838e020e064f947716befab787356a66b62b7bd1
ocornut <omarcornut@gmail.com>
2020-08-17 13:25:27 +0200

    Tables: Fixed stacked popups incorrectly accessing g.CurrentTable of parent-in-stack windows.

--- imgui.cpp
+++ imgui.cpp
@@ -2932,6 +2932,7 @@ static void SetCurrentWindow(ImGuiWindow* window)
 {
     ImGuiContext& g = *GImGui;
     g.CurrentWindow = window;
+    g.CurrentTable = window ? window->DC.CurrentTable : NULL;
     if (window)
         g.FontSize = g.DrawListSharedData.FontSize = window->CalcFontSize();
 }
